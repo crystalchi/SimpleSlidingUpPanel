c8e677dbacd608f26474c126da77e1fad0e401d7
crystalchi <xp_chi@163.com>
2016-11-29 10:13:11 +0800

    code over.

--- app/src/main/java/com/crystal/simpleslidinguppanel/MainActivity.java
+++ app/src/main/java/com/crystal/simpleslidinguppanel/MainActivity.java
@@ -16,15 +16,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_drag);
 
-        mLvMainContent = (ListView) findViewById(R.id.lv_mainview);
+        /*mLvMainContent = (ListView) findViewById(R.id.lv_mainview);
         mLvList = (ListView) findViewById(R.id.lv_dragview);
         List<String> mainContentList = new ArrayList<String>();
         for(int i = 1; i <= 100; i++){
             mainContentList.add("crystal" + i);
         }
         mLvMainContent.setAdapter(new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, mainContentList));
-        mLvList.setAdapter(new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, mainContentList));
+        mLvList.setAdapter(new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, mainContentList));*/
     }
 }
